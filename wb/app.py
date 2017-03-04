e5e8ca3605093284491365031ef7d00da1e93f32
john pinkston <pinkstonjack2@gmail.com>
2017-03-04 15:44:52 -0600

    refactored for pep

--- wb/app.py
+++ wb/app.py
@@ -1,56 +1,51 @@
-import sys
-import os
-import json
-from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout,
-							 QHBoxLayout, QLabel, QLineEdit, QTabBar,
-							 QFrame, QStackedLayout)
-from PyQt5.QtGui import QIcon, QWindow, QImage
-from PyQt5.QtCore import *
-from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWidgets import QVBoxLayout
 from . import tab
 from . import address
 from . import toolbar
 from . import mainView
 
+
 class CreateApp:
-	"""
-	CreateApp is where all pieces are put togethor to build the app
-	Methods:
-		__init__
-		createLayout
-	"""
+    """
+    CreateApp is where all pieces are put togethor to build the app
+    Methods:
+        __init__
+        create_layout
+    """
 
-	def __init__(self):
-		self.createLayout()
+    def __init__(self):
+        self.create_layout()
 
-	def createLayout(self):
-		"""
-		createLayout sets the tabbar, main container, and toolbar(address bar is in toolbar)
-		inside the layout
-		"""
-		self.layout = QVBoxLayout()
-		self.layout.setSpacing(0)
-		self.layout.setContentsMargins(0, 0, 0, 0)
+    def create_layout(self):
+        """
+        createLayout sets the tabbar, main container, and toolbar(address bar is in toolbar)
+        inside the layout
+        """
+        self.layout = QVBoxLayout()
+        self.layout.setSpacing(0)
+        self.layout.setContentsMargins(0, 0, 0, 0)
 
-		self.tabbar = tab.TabBar()
+        self.tabbar = tab.TabBar()
 
-		self.addressBar = address.AddressBar()
+        self.addressBar = address.AddressBar()
 
-		self.toolbar = toolbar.ToolBar()
-		self.toolbar.toolbarLayout.addWidget(self.addressBar)
-		self.toolbar.toolbarLayout.addWidget(self.tabbar.addTabButton)
+        self.toolbar = toolbar.ToolBar()
+        self.toolbar.toolbarLayout.addWidget(self.addressBar)
+        self.toolbar.toolbarLayout.addWidget(self.tabbar.addTabButton)
 
-		self.container = mainView.Container()
+        self.container = mainView.Container()
 
-		self.layout.addWidget(self.tabbar.tabbar)
-		self.layout.addWidget(self.toolbar.toolbar)
-		self.layout.addWidget(self.container.container)
+        self.layout.addWidget(self.tabbar.tabbar)
+        self.layout.addWidget(self.toolbar.toolbar)
+        self.layout.addWidget(self.container.container)
 
-		self.container.container.layout.addWidget(self.tabbar.tabs[0])
-		self.container.container.layout.setCurrentWidget(self.tabbar.tabs[0])
-		self.tabbar.tabbar.addTab("New Tab")
-		self.tabbar.tabbar.setTabData(0, 'tab' + str(0))
+        self.container.container.layout.addWidget(self.tabbar.tabs[self.tabbar.tabCount])
+        self.container.container.layout.setCurrentWidget(self.tabbar.tabs[self.tabbar.tabCount])
+        self.tabbar.tabbar.addTab("New Tab")
+        self.tabbar.tabbar.setTabData(self.tabbar.tabCount, 'tab' + str(self.tabbar.tabCount))
 
-
-
-
+    def update_layout(self):
+        self.container.container.layout.addWidget(self.tabbar.tabs[self.tabbar.tabCount])
+        self.container.container.layout.setCurrentWidget(self.tabbar.tabs[self.tabbar.tabCount])
+        self.tabbar.tabbar.addTab("New Tab")
+        self.tabbar.tabbar.setTabData(self.tabbar.tabCount, 'tab' + str(self.tabbar.tabCount))
--- wb/tab.py
+++ wb/tab.py
@@ -1,53 +1,57 @@
-import sys
-import os
-import json
-from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout,
-							 QHBoxLayout, QLabel, QLineEdit, QTabBar,
-							 QFrame, QStackedLayout, QPushButton)
-from PyQt5.QtGui import QIcon, QWindow, QImage
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTabBar, QPushButton
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 from . import app
 
+
 class TabBar:
-	"""
-	TabBar builds the initial default tab loaded with google, and builds the button and functionality
-	to add a newtab(which by default will load google)
-	Methods:
-		__init__
-		CloseTab
-		addTabs
-	"""
-	def __init__(self):
-		self.tabbar = QTabBar(movable=True, tabsClosable=True)
-		self.tabbar.tabCloseRequested.connect(self.CloseTab)
-		self.addTabButton = QPushButton("+")
-		self.addTabButton.clicked.connect(self.addTabs)
-		self.tabCount = 0
-		self.tabs = []
-		self.addTabs()
+    """
+    TabBar builds the initial default tab loaded with google, and builds the button and functionality
+    to add a newtab(which by default will load google)
+    Methods:
+        __init__
+        close_tab
+        add_tab
+    """
+    def __init__(self):
+        self.tabbar = QTabBar(movable=True, tabsClosable=True)
+        self.tabbar.tabCloseRequested.connect(self.close_tab)
+        self.addTabButton = QPushButton("+")
+        self.addTabButton.clicked.connect(self.add_new_tabs)
+        self.tabCount = 0
+        self.tabs = []
+        self.add_tab()
 
-	def CloseTab(self, j):
-		"""
-		Close tabs closes the selected tab based on index
-		Arguments:
-			j = index of tab you want to remove
-		"""
-		self.tabbar.removeTab(j)
+    def close_tab(self, j):
+        """
+        Close tabs closes the selected tab based on index
+        Arguments:
+            j = index of tab you want to remove
+        """
+        self.tabbar.removeTab(j)
 
-	def addTabs(self):
-		"""
-		addTabs builds the initial tab and has the functionality to create additional tabs when 
-		the newtab button is pressed
-		"""
-		j = self.tabCount
-		self.tabs.append(QWidget())
-		self.tabs[j].layout = QVBoxLayout()
-		self.tabs[j].setObjectName('Tab ' + str(j))
-		self.tabs[j].content = QWebEngineView()
-		self.tabs[j].content.load(QUrl.fromUserInput("http://google.com"))
-		self.tabs[j].layout.addWidget(self.tabs[j].content)
-		self.tabs[j].setLayout(self.tabs[j].layout)
-		self.tabbar.setCurrentIndex(0)
+    def add_tab(self):
+        """
+        addTabs builds the initial tab and has the functionality to create additional tabs when
+        the newtab button is pressed
+        """
+        # self.tabCount += 1
+        self.tabs.append(QWidget())
+        self.tabs[self.tabCount].layout = QVBoxLayout()
+        self.tabs[self.tabCount].setObjectName('Tab ' + str(self.tabCount))
+        self.tabs[self.tabCount].content = QWebEngineView()
+        self.tabs[self.tabCount].content.load(QUrl.fromUserInput("http://google.com"))
+        self.tabs[self.tabCount].layout.addWidget(self.tabs[self.tabCount].content)
+        self.tabs[self.tabCount].setLayout(self.tabs[self.tabCount].layout)
+        # self.tabbar.setCurrentIndex(0)
 
-		
+    def add_new_tabs(self):
+        self.tabCount += 1
+        self.tabs.append(QWidget())
+        self.tabs[self.tabCount].layout = QVBoxLayout()
+        self.tabs[self.tabCount].setObjectName('Tab ' + str(self.tabCount))
+        self.tabs[self.tabCount].content = QWebEngineView()
+        self.tabs[self.tabCount].content.load(QUrl.fromUserInput("http://google.com"))
+        self.tabs[self.tabCount].layout.addWidget(self.tabs[self.tabCount].content)
+        self.tabs[self.tabCount].setLayout(self.tabs[self.tabCount].layout)
+        app.CreateApp().update_layout()
