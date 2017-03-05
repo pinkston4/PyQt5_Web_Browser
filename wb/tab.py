a804e5648800805f09f05c2125af47e75d5d7555
john pinkston <pinkstonjack2@gmail.com>
2017-03-04 22:29:58 -0600

    refactored

--- wb/tab.py
+++ wb/tab.py
@@ -1,57 +1,28 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTabBar, QPushButton
+from PyQt5.QtWidgets import QTabWidget, QVBoxLayout, QTabBar
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
-from . import app
 
 
 class TabBar:
     """
-    TabBar builds the initial default tab loaded with google, and builds the button and functionality
-    to add a newtab(which by default will load google)
+    tab bar is where the web engine ties into place
+    the tabbar is built with one tab, none of which will be displayed on screen
     Methods:
         __init__
-        close_tab
         add_tab
+    Author: Jack Pinkston
     """
     def __init__(self):
-        self.tabbar = QTabBar(movable=True, tabsClosable=True)
-        self.tabbar.tabCloseRequested.connect(self.close_tab)
-        self.addTabButton = QPushButton("+")
-        self.addTabButton.clicked.connect(self.add_new_tabs)
-        self.tabCount = 0
-        self.tabs = []
+        self.tabbar = QTabBar()
+        self.tab = QTabWidget()
         self.add_tab()
-
-    def close_tab(self, j):
-        """
-        Close tabs closes the selected tab based on index
-        Arguments:
-            j = index of tab you want to remove
-        """
-        self.tabbar.removeTab(j)
 
     def add_tab(self):
         """
-        addTabs builds the initial tab and has the functionality to create additional tabs when
-        the newtab button is pressed
+        add_tab generates the tab and loads the url for that tab
         """
-        # self.tabCount += 1
-        self.tabs.append(QWidget())
-        self.tabs[self.tabCount].layout = QVBoxLayout()
-        self.tabs[self.tabCount].setObjectName('Tab ' + str(self.tabCount))
-        self.tabs[self.tabCount].content = QWebEngineView()
-        self.tabs[self.tabCount].content.load(QUrl.fromUserInput("http://google.com"))
-        self.tabs[self.tabCount].layout.addWidget(self.tabs[self.tabCount].content)
-        self.tabs[self.tabCount].setLayout(self.tabs[self.tabCount].layout)
-        # self.tabbar.setCurrentIndex(0)
-
-    def add_new_tabs(self):
-        self.tabCount += 1
-        self.tabs.append(QWidget())
-        self.tabs[self.tabCount].layout = QVBoxLayout()
-        self.tabs[self.tabCount].setObjectName('Tab ' + str(self.tabCount))
-        self.tabs[self.tabCount].content = QWebEngineView()
-        self.tabs[self.tabCount].content.load(QUrl.fromUserInput("http://google.com"))
-        self.tabs[self.tabCount].layout.addWidget(self.tabs[self.tabCount].content)
-        self.tabs[self.tabCount].setLayout(self.tabs[self.tabCount].layout)
-        app.CreateApp().update_layout()
+        self.tab.layout = QVBoxLayout()
+        self.tab.content = QWebEngineView()
+        self.tab.content.load(QUrl.fromUserInput("http://google.com"))
+        self.tab.layout.addWidget(self.tab.content)
+        self.tab.setLayout(self.tab.layout)
--- wb/toolbar.py
+++ wb/toolbar.py
@@ -2,6 +2,10 @@ from PyQt5.QtWidgets import QWidget, QHBoxLayout
 
 
 class ToolBar:
+    """
+    the TooBar class sets the layout for the toolbar, a horizontal bar
+    only method is __init__
+    """
 
     def __init__(self):
         self.toolbar = QWidget()
